ae2e1062696a0f3c6e830435b44536d2b15667f8
Asad Waheed <asadwaheed@cmail.carleton.ca>
2020-11-21 16:53:56 -0500

    Feat: Fixed importing issue

--- tests/test_tempsensor.py
+++ tests/test_tempsensor.py
@@ -3,9 +3,9 @@ import logging
 import RPi.GPIO as GPIO
 from unittest import TestCase, main
 from unittest.mock import patch, call, MagicMock
-from tempsensor.tempsensor.py import TempSensor
-from tempsensor.fan.py import Fan
-from tempsensor.temp.py import Temperature
+from tempsensor.tempsensor import TempSensor
+from tempsensor.fan import Fan
+from tempsensor.temp import Temperature
 
 class TestTempSensor(TestCase):
 	def setUp(self):
@@ -14,13 +14,13 @@ class TestTempSensor(TestCase):
 		self.fan_mock.__fan_on = False
 		self.temp_sensor = TempSensor(temp = self.temp_mock, fan = self.fan_mock)
 
-	def test_check_and_update_status_true(self):
-		self.temp_mock.check_input.return_value = True
-		self.assertTrue(self.temp_sensor.check_and_update_status())
+	def test_checking_status_true(self):
+		self.temp_mock.read_data.return_value = True
+		self.assertTrue(self.temp_sensor.checking_status())
 
-	def test_check_and_update_status_false(self):
-		self.temp_mock.check_input.return_value = False
-		self.assertFalse(self.temp_sensor.check_and_update_status())
+	def test_checking_status_false(self):
+		self.temp_mock.read_data.return_value = False
+		self.assertFalse(self.temp_sensor.checking_status())
 
 @patch("RPi.GPIO.output", autospec = True)
 class TestFan(TestCase):
@@ -34,21 +34,24 @@ class TestFan(TestCase):
 
 	def test_hot_status(self, mock_output):
 		"""
-		Testing FAN OFF to ON
+		TESTING FAN TURNING OFF TO ON
 		"""
 		self.fan.room_status(True)
-		status = self.fan.hot_status()
+		status = self.fan.hot_status(True)
 		err_msg = "FAN status did not change from off to on"
-		self.assertTrue(status, err_msg)
+		self.assertFalse(status, err_msg)
 
 		calls = [call(self.pin, GPIO.LOW), call(self.pin, GPIO.HIGH)]
 		mock_output.assert_has_calls(calls)
 
 	def test_cold_status(self, mock_output):
+		"""
+		TESTING FAN TURNING ON TO OFF
+		"""
 		self.fan.hot_status(True)
-		status = self.fan.cold_status()
+		status = self.fan.cold_status(True)
 		err_msg = "FAN status did not change from on to off"
-		self.assertTrue(status, err_msg)
+		self.assertFalse(status, err_msg)
 
 		calls = [call(self.pin, GPIO.HIGH), call(self.pin, GPIO.LOW)]
 		mock_output.assert_has_calls(calls)
@@ -69,7 +72,7 @@ class TestTemperature(TestCase):
 	def test_check_input_not_detected(self, mock_input):
 		mock_input.return_value = False
 		err_msg = "Temp Sensor detected temperature"
-		self.assertFalse(self.temp.read_data(), err_msg)
+		self.assertTrue(self.temp.read_data(), err_msg)
 
 if __name__ == "__main__":
 	logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level = logging.INFO)
