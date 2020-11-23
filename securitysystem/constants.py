6119d35f87e5cf696888f890b87d6ace45bb324f
Hasan Baig <hasanbaig2000@gmail.com>
2020-11-23 15:33:35 -0500

    feat: adding constants & removing magic numbers

--- securitysystem/constants.py
+++ securitysystem/constants.py
@@ -1,6 +1,8 @@
 """
-Move API keys to database (temp file)
+Constant values used throughout node (ensures no magic numbers)
 """
+from logging import DEBUG, INFO
+
 # ThingSpeak Channel for HomePixel SecuritySystem
 L2_M_5A1_WRITE_KEY = '4ZYI2SW67KJBJR6A'
 L2_M_5A1_READ_KEY = 'RZGQ9FXZIQX5LZUP'
@@ -11,10 +13,22 @@ L2_M_5A2_WRITE_KEY = 'B8IMPW6Y5OSC47MJ'
 L2_M_5A2_READ_KEY = '88DYKYTO4OQ2BT2U'
 L2_M_5A2_FEED = '1152488'
 
+# ThingSpeak Field Constants for Production
+LOCATION_FIELD = 'field1'
+NODE_ID_FIELD = 'field2'
+DATE_TIME_FIELD = 'field3'
+
+# ThingSpeak Field Constants for Testing
+TEST_FIELD = 'field1'
+
 # URL Syntax for LightClapper
 READ_URL = 'https://api.thingspeak.com/channels/' + \
-           '{CHANNEL_FEED}/fields/1.json?api_key=' + \
-           '{READ_KEY}&results={HEADER}'
+           '{CHANNEL_FEED}/feeds.json?api_key=' + \
+           '{READ_KEY}'
+
+READ_URL_LIMITED = 'https://api.thingspeak.com/channels/' + \
+                   '{CHANNEL_FEED}/feeds.json?api_key=' + \
+                   '{READ_KEY}&results={RESULTS}'
 
 # SMS API using Nexmo
 SMS_API_KEY = '2d73a813'
@@ -28,6 +42,19 @@ SMTP_PORT = 587
 GMAIL_USERNAME = 'sysc3010homepixel@gmail.com' 
 GMAIL_PASSWORD = 'homepixel3010'
 
+
+# SecuritySystem DB constants
+SECURITY_SYSTEM_DB = 'securitysystem.db'
+SECURITY_SYSTEM_NAME = 'SecuritySystem'
+
+# Logging Constants
+LOGGING_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+LOGGING_DEFAULT_LEVEL = DEBUG
+LOGGING_TEST_LEVEL = INFO
+
+# Testing constants
+MOTION_DETECTED = True
+MOTION_NOT_DETECTED = False
 
 # Other constants
 GOOD_STATUS = 200
