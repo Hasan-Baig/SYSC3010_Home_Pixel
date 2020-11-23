2731279717e1c43a71455cdb1144966b105c4e9b
Hasan Baig <hasanbaig2000@gmail.com>
2020-11-23 15:30:36 -0500

    feat: shell script to set env and run unit tests

---
+++ homepixel_env.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "                              ___ _          _ "
+echo "  /\  /\___  _ __ ___   ___  / _ (_)_  _ ___| |"
+echo " / /_/ / _ \|  _ ' _ \ / _ \/ /_)/ \ \/ / _ \ |"
+echo "/ __  / (_) | | | | | |  __/ ___/| |>  <  __/ |"
+echo "\/ /_/ \___/|_| |_| |_|\___\/    |_/_/\_\___|_|"
+
+echo "\nSetting up environment..."
+export PYTHONPATH=":$PWD/lightclapper:$PWD/securitysystem:$PWD/tempsensor"
+echo "PYTHONPATH =" $PYTHONPATH
+echo "Environment ready. Have a nice day!"
---
+++ securitysystem_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Script to run SecuritySystem node unittests
+echo "Running SecuritySystem node unittests..."
+python3 tests/test_securitysystem.py -v
+python3 tests/test_securitysystemclient.py -v
+python3 tests/test_securitysystemdb.py -v
+python3 tests/test_thingspeak.py -v
