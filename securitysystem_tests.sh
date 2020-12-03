18c98c4b04c54cf040d38c4cc03adc6ed617f6ff
Hasan Baig <hasanbaig2000@gmail.com>
2020-12-03 15:16:14 -0500

    test: tests updated

--- securitysystem_tests.sh
+++ securitysystem_tests.sh
@@ -4,4 +4,4 @@ echo "Running SecuritySystem node unittests..."
 python3 tests/test_securitysystem.py -v
 python3 tests/test_securitysystemclient.py -v
 python3 tests/test_securitysystemdb.py -v
-python3 tests/test_securitythingspeak.py -v
+python3 tests/test_securitysystemthingspeak.py -v
