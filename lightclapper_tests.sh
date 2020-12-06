17918980330c9165e4ad7dff465bb89b660355a5
Mariana Rafael-White <marianarafaelwhite@cmail.carleton.ca>
2020-12-06 18:54:17 -0500

    Rename LightClapperThingSpeak test
    This commit renames the ThingSpeak test
    used for verifying the LightClapper thingspeak
    functions.

--- lightclapper_tests.sh
+++ lightclapper_tests.sh
@@ -4,4 +4,4 @@ echo "Running LightClapper node unittests..."
 python3 tests/test_lightclapper.py -v
 python3 tests/test_lightclapperclient.py -v
 python3 tests/test_lightclapperdb.py -v
-python3 tests/test_thingspeak.py -v
+python3 tests/test_lightclapperthingspeak.py -v
