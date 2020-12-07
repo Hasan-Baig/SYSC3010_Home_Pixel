b1625df66dff483408fbdaae8e7436136e5a5780
Hasan Baig <43944570+Hasan-Baig@users.noreply.github.com>
2020-12-06 23:31:18 -0500

    feat: setting up environment variables

--- homepixel_env.sh
+++ homepixel_env.sh
@@ -8,5 +8,11 @@ echo "\/ /_/ \___/|_| |_| |_|\___\/    |_/_/\_\___|_|"
 
 echo "\nSetting up environment..."
 export PYTHONPATH=":$PWD/lightclapper:$PWD/securitysystem:$PWD/tempsensor"
+export FLASK_APP="main.py"
+export FLASK_ENV="development"
+export FLASK_DEBUG="1"
 echo "PYTHONPATH =" $PYTHONPATH
+echo "FLASK_APP =" $FLASK_APP
+echo "FLASK_ENV =" $FLASK_ENV
+echo "FLASK_DEBUG =" $FLASK_DEBUG
 echo "Environment ready. Have a nice day!"
