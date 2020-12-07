0a198b5cd5fbc779f9d2d169cf8c7899340b0e72
Asad Waheed <asadwaheed@cmail.carleton.ca>
2020-12-06 19:05:30 -0500

    Test: TempSensor node tests

---
+++ tempsensor_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#Script to run TempSensor node unittests
+echo "Running TempSensor node unittests..."
+python3 tests/test_tempsensor.py -v
+python3 tests/test_tempsensorclient.py -v
+python3 tests/test_testtempsensordb.py -v
+python3 tests/test_tempthingspeak.py -v
